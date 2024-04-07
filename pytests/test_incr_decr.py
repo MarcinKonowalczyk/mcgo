9edd1664a7d3fdf8de182d571e740944727bf06a
Marcin <marcin.konow@lczyk.xyz>
2024-04-07 18:48:22 +0100

    adjusted the C implementation to be a bit more intuitive on incr/decr

--- pytests/test_incr_decr.py
+++ pytests/test_incr_decr.py
@@ -1,7 +1,6 @@
 import pytest
 from pymemcache.client.base import Client
-
-from conftest import is_go_impl
+from pymemcache.exceptions import MemcacheClientError
 
 
 def test_incr_decr(client: Client) -> None:
@@ -16,17 +15,37 @@ def test_incr_decr(client: Client) -> None:
     assert result == b"13"
 
 
-def test_decr_unsigned(client: Client) -> None:
+def test_decr_below_zero(client: Client) -> None:
+    client.set("hello", 0)
+    assert client.get("hello") == b"0"
+    result = client.decr("hello", 1)
+    assert result == -1
+    assert client.get("hello") == b"-1"
+
+
+def test_number_format(client: Client) -> None:
     """Test decrementing a key below zero. The value should be set to 0."""
 
-    client.set("hello", 1)
-    result = client.decr("hello", 1)
-    assert result == 0
-    assert client.get("hello") == b"0"
+    for value in (1, "1", "+1"):
+        client.set("hello", value)
+        assert client.get("hello") == str(value).encode()
+        result = client.incr("hello", 1)
+        assert result == 2
+        assert client.get("hello") == b"2"
 
-    result = client.decr("hello", 4)
-    assert result == 0
-    assert client.get("hello") == b"0"
+    for value in (0, "0", "+0", "-0"):
+        client.set("hello", value)
+        assert client.get("hello") == str(value).encode()
+        result = client.incr("hello", 1)
+        assert result == 1
+        assert client.get("hello") == b"1"
+
+    for value in (-1, "-1"):
+        client.set("hello", value)
+        assert client.get("hello") == str(value).encode()
+        result = client.incr("hello", 1)
+        assert result == 0
+        assert client.get("hello") == b"0"
 
 
 def test_incr_decr_not_found(client: Client) -> None:
@@ -57,28 +76,22 @@ def test_incr_decr_noreply(client: Client) -> None:
     assert result == b"2"
 
 
-# @pytest.mark.c_impl
-def test_incr_decr_non_numeric_c(client: Client) -> None:
+def test_incr_decr_non_numeric(client: Client) -> None:
     """Test behaviour on incrementing and decrementing a non-numeric key. The key should be treated as 0."""
-    client.set("hello", "abcd")
-    result = client.incr("hello", 2)
-    assert result == 2
-    result = client.get("hello")
-    assert result == b"2   "
+    client.set("hello", "world")
+    with pytest.raises(MemcacheClientError):
+        client.incr("hello", 2)
 
-    # DECR on non-numeric decrements
-    client.set("hello", "abcd")
+    assert client.get("hello") == b"world"
+
+    with pytest.raises(MemcacheClientError):
+        client.decr("hello", 2)
+
+    assert client.get("hello") == b"world"
+
+
+def test_incr_behaviour(client: Client) -> None:
+    # We still parse
+    client.set("hello", 0)
     result = client.decr("hello", 2)
-    assert result == 0
-    result = client.get("hello")
-    assert result == b"0   "
-
-
-# @pytest.mark.go_impl
-# def test_incr_decr_non_numeric_go(client: Client) -> None:
-#     # INCR on non-numeric does not increment
-#     client.set("hello", "abcd")
-#     result = client.incr("hello", 2)
-#     assert result is None
-#     result = client.get("hello")
-#     assert result == b"abcd"
+    assert result == -2
