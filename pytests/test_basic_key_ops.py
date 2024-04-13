74d3ef3b4b0002a7b7e04061cb374f0b8f94bee9
Marcin <marcin.konow@lczyk.xyz>
2024-04-13 11:18:11 +0100

    test

--- pytests/test_basic_key_ops.py
+++ pytests/test_basic_key_ops.py
@@ -39,3 +39,9 @@ def test_key_not_found(client: Client) -> None:
     # Test getting a key which never existed
     result = client.get("non_existent_key")
     assert result is None
+
+
+def test_set_noreply(client: Client) -> None:
+    client.set("hello", "world", noreply=True)
+    result = client.get("hello")
+    # assert result == b"world"
